1ff0fb760ec58f402f1eddd3dad97d2d7983ec31
Allan Blanchard <a.blanchard@iia-laval.fr>
2020-02-10 09:51:27 +0100

    display copyright

--- src/application.bat
+++ src/application.bat
@@ -1,3 +1,4 @@
- ### application.bat - 2020 - Blanchard Allan ###
+### application.bat - 2020 - Blanchard Allan ###
+echo "Copyright company - 2020"
 echo "Build : 10:00"
-echo "version 1"
+echo "version 1.1"
